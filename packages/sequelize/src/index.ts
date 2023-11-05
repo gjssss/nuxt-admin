c891a3216aa245d3808b64c3e188f2a80f9509e5
gjssss <gaojinsong2010@gmail.com>
2023-11-05 16:34:21 +0800

    feat:✨ 使用Nuxt Modules自动注册model

--- packages/sequelize/src/index.ts
+++ packages/sequelize/src/index.ts
@@ -1,12 +1,54 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { addServerPlugin, addTemplate, addTypeTemplate, createResolver, defineNuxtModule, resolveFiles } from '@nuxt/kit'
+import { genArrayFromRaw, genImport, genSafeVariableName } from 'knitwork'
+import defu from 'defu'
+import { getNameFromPath } from './utils/path'
 
 export default defineNuxtModule({
   meta: {
-    name: 'nuxt-sequelize',
+    name: '#nuxt/sequelize',
   },
-  setup(options, nuxt) {
-    nuxt.hook('modules:done', () => {
-      console.log('My module is ready with current test option: ')
+  async setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url)
+
+    // const modelFiles = await resolveFiles(nuxt.options.srcDir, 'model/*')
+    // const appModels = modelFiles.map(path => ({
+    //   name: getNameFromPath(path),
+    //   path,
+    // }))
+
+    // virtual imports
+    nuxt.hook('nitro:config', (_config) => {
+      _config.alias = _config.alias || {}
+
+      // Inline module runtime in Nitro bundle
+      _config.externals = defu(typeof _config.externals === 'object' ? _config.externals : {}, {
+        inline: [resolve('./runtime/service')],
+      })
+      _config.alias['#nuxt/sequelize'] = resolve('./runtime/service')
+
+      if (_config.imports) {
+        _config.imports.dirs = _config.imports.dirs || []
+        _config.imports.dirs?.push('model')
+      }
     })
+
+    addTypeTemplate({
+      filename: 'types/nuxt-sequelize.d.ts',
+      getContents: () => [
+        'declare module \'#nuxt/sequelize\' {',
+        `  const defineSequelizeModel: typeof import('${resolve('./runtime/service')}').defineSequelizeModel`,
+        '}',
+      ].join('\n'),
+    })
+
+    // addTemplate({
+    //   filename: 'model.mjs',
+    //   getContents: () => [
+    //     ...appModels.map(item =>
+    //       genImport(item.path, genSafeVariableName(item.name)),
+    //     ),
+    //   `export const mysqlModels = ${genArrayFromRaw(appModels.map(item => genSafeVariableName(item.name)))}`,
+    //   ].join('\n'),
+    // })
   },
 })
